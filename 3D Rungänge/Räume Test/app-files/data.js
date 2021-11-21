83e434685921866cf8c5e40c1a7016c07107e549
Fiona <fiona.noemi.virnich@hs-furtwangen.de>
2021-11-21 13:01:33 +0100

    test räume

---
+++ 3D Rungänge/Räume Test/app-files/data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kreativraum-tuttlingen",
+      "name": "Kreativraum Tuttlingen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "yaw": 0.5912744152670122,
+        "pitch": -0.11867200849241755,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.694798439549995,
+          "pitch": 0.059162778989870546,
+          "rotation": 0,
+          "target": "1-makerspace-tuttlingen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.33244839630286904,
+          "pitch": 0.3141833222802397,
+          "title": "Kreativraum",
+          "text": "Tuttlingen"
+        }
+      ]
+    },
+    {
+      "id": "1-makerspace-tuttlingen",
+      "name": "Makerspace Tuttlingen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "yaw": 0.051445205277079964,
+        "pitch": 0.20183148222146485,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.277361587641664,
+          "pitch": 0.08529308531351631,
+          "rotation": 0,
+          "target": "0-kreativraum-tuttlingen"
+        },
+        {
+          "yaw": 2.6760373597150267,
+          "pitch": -0.1721931846638931,
+          "rotation": 0,
+          "target": "2-halle-schwenningen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5269560132100661,
+          "pitch": 0.4905193230698792,
+          "title": "Makerspace",
+          "text": "Tuttlingen"
+        }
+      ]
+    },
+    {
+      "id": "2-halle-schwenningen",
+      "name": "Halle Schwenningen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8762591734939313,
+          "pitch": 0.07833465722563382,
+          "rotation": 0,
+          "target": "1-makerspace-tuttlingen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.008615892717568,
+          "pitch": -0.004836355463702091,
+          "title": "Kreativraum",
+          "text": "Schwenningen, Halle"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
