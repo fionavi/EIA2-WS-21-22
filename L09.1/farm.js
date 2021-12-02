a955d3a2661d2346ab6533bda77e7504fe5ba8f9
Fiona <fiona.noemi.virnich@hs-furtwangen.de>
2021-12-02 18:10:49 +0100

    aufgabe fertig

--- L09.1/farm.js
+++ L09.1/farm.js
@@ -1,18 +1,17 @@
 "use strict";
 var L09_Farm;
 (function (L09_Farm) {
+    let crc2;
     let animals = [];
     let storageLeft = {
-        hay: 500,
-        meat: 100,
-        mice: 50,
-        corn: 300,
-        grass: 200
+        hay: 100,
+        meat: 20,
+        mice: 10,
+        corn: 30,
+        grass: 40
     };
     console.log(storageLeft.hay);
-    window.addEventListener("load", handleload);
-    let crc2;
-    function handleload(_event) {
+    window.addEventListener("load", function () {
         let canvas = document.querySelector("canvas");
         if (!canvas) {
             return;
@@ -23,8 +22,8 @@ var L09_Farm;
         createAnimals();
         feedAnimals();
         singAnimals();
-        // window.setInterval(update, 50);
-    }
+        document.querySelector("#day").addEventListener("click", feedAnimals);
+    });
     function createAnimals() {
         console.log("creating Animals");
         let cow = new L09_Farm.Animal("cow", "hay", 50, "Mooo!");
@@ -42,13 +41,55 @@ var L09_Farm;
         console.log("I am a " + cat.species);
         console.log("I am a " + chicken.species);
         console.log("I am a " + sheep.species);
+        let cowDiv = document.getElementById("cow");
+        let dogDiv = document.getElementById("dog");
+        let catDiv = document.getElementById("cat");
+        let chickenDiv = document.getElementById("chicken");
+        let sheepDiv = document.getElementById("sheep");
+        cowDiv.innerHTML = "I am a " + cow.species + "<br>";
+        dogDiv.innerHTML = "I am a " + dog.species + "<br>";
+        catDiv.innerHTML = "I am a " + cat.species + "<br>";
+        chickenDiv.innerHTML = "I am a " + chicken.species + "<br>";
+        sheepDiv.innerHTML = "I am a " + sheep.species + "<br>";
     }
     function feedAnimals() {
+        console.log("button pressed");
         console.log("I ate " + animals[0].amount + "kg of " + animals[0].food);
         console.log("I ate " + animals[1].amount + "kg of " + animals[1].food);
         console.log("I ate " + animals[2].amount + " " + animals[2].food);
         console.log("I ate " + animals[3].amount + "kg of " + animals[3].food);
         console.log("I ate " + animals[4].amount + "kg of " + animals[4].food);
+        let cowDiv = document.getElementById("cow");
+        let dogDiv = document.getElementById("dog");
+        let catDiv = document.getElementById("cat");
+        let chickenDiv = document.getElementById("chicken");
+        let sheepDiv = document.getElementById("sheep");
+        cowDiv.innerHTML += "<br>" + "I ate " + animals[0].amount + "kg of " + animals[0].food + "<br>";
+        dogDiv.innerHTML += "<br>" + "I ate " + animals[1].amount + "kg of " + animals[1].food + "<br>";
+        catDiv.innerHTML += "<br>" + "I ate " + animals[2].amount + "kg of " + animals[2].food + "<br>";
+        chickenDiv.innerHTML += "<br>" + "I ate " + animals[3].amount + "kg of " + animals[3].food + "<br>";
+        sheepDiv.innerHTML += "<br>" + "I ate " + animals[4].amount + "kg of " + animals[4].food + "<br>";
+        feedAnimals();
+        displayStorage();
+    }
+    function singAnimals() {
+        console.log("Old Mc Donalds had a farm. " + animals[0].sound + " " + animals[0].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[1].sound + " " + animals[1].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[2].sound + " " + animals[2].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[3].sound + " " + animals[3].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[4].sound + " " + animals[4].sound);
+        let cowDiv = document.getElementById("cow");
+        let dogDiv = document.getElementById("dog");
+        let catDiv = document.getElementById("cat");
+        let chickenDiv = document.getElementById("chicken");
+        let sheepDiv = document.getElementById("sheep");
+        cowDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[0].sound + " " + animals[0].sound;
+        dogDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[1].sound + " " + animals[1].sound;
+        catDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[2].sound + " " + animals[2].sound;
+        chickenDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[3].sound + " " + animals[3].sound;
+        sheepDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[4].sound + " " + animals[4].sound;
+    }
+    function displayStorage() {
         storageLeft.hay -= 50;
         console.log("You have " + storageLeft.hay + " kg hay left.");
         storageLeft.meat -= 3;
@@ -59,13 +100,23 @@ var L09_Farm;
         console.log("You have " + storageLeft.corn + " kg corn left.");
         storageLeft.grass -= 10;
         console.log("You have " + storageLeft.grass + " kg grass left.");
-    }
-    function singAnimals() {
-        console.log("Old Mc Donalds had a farm. " + animals[0].sound + " " + animals[0].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[1].sound + " " + animals[1].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[2].sound + " " + animals[2].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[3].sound + " " + animals[3].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[4].sound + " " + animals[4].sound);
+        let storageDiv = document.getElementById("storage");
+        storageDiv.innerHTML = "Old Mc Donalds, here is what you have left:" + "<br>" + "<br>" + storageLeft.hay + " kg of hay " + "<br>" + storageLeft.meat + " kg of meat " + "<br>" + storageLeft.mice + " mice " + "<br>" + storageLeft.corn + " kg of corn " + "<br>" + storageLeft.grass + " kg of grass" + "<br>";
+        if (storageLeft.hay <= 0) {
+            alert("Get more hay!");
+        }
+        if (storageLeft.meat <= 0) {
+            alert("Buy more meat!");
+        }
+        if (storageLeft.mice <= 0) {
+            alert("Catch more mice!");
+        }
+        if (storageLeft.corn <= 0) {
+            alert("Grow more corn!");
+        }
+        if (storageLeft.corn <= 0) {
+            alert("Get more grass!");
+        }
     }
 })(L09_Farm || (L09_Farm = {}));
 //# sourceMappingURL=farm.js.map
--- L09.1/farm.ts
+++ L09.1/farm.ts
@@ -1,5 +1,7 @@
 namespace L09_Farm {
 
+    let crc2: CanvasRenderingContext2D;
+
     let animals: Animal[] = [];
 
     interface Storage {
@@ -11,21 +13,16 @@ namespace L09_Farm {
     }
 
     let storageLeft: Storage = {
-        hay: 500,
-        meat: 100,
-        mice: 50,
-        corn: 300,
-        grass: 200
+        hay: 100,
+        meat: 20,
+        mice: 10,
+        corn: 30,
+        grass: 40
     };
 
     console.log(storageLeft.hay);
 
-
-
-    window.addEventListener("load", handleload);
-    let crc2: CanvasRenderingContext2D;
-
-    function handleload(_event: Event): void {
+    window.addEventListener("load", function(): void {
         let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
         if (!canvas) {
             return;
@@ -37,9 +34,11 @@ namespace L09_Farm {
         createAnimals();
         feedAnimals();
         singAnimals();
+        
+        document.querySelector("#day").addEventListener("click", feedAnimals);
+    });
 
-        // window.setInterval(update, 50);
-    }
+    
 
 
     function createAnimals(): void {
@@ -62,10 +61,25 @@ namespace L09_Farm {
         console.log("I am a " + cat.species);
         console.log("I am a " + chicken.species);
         console.log("I am a " + sheep.species);
+
+        let cowDiv: HTMLElement = document.getElementById("cow");
+        let dogDiv: HTMLElement = document.getElementById("dog");
+        let catDiv: HTMLElement = document.getElementById("cat");
+        let chickenDiv: HTMLElement = document.getElementById("chicken");
+        let sheepDiv: HTMLElement = document.getElementById("sheep");
+
+
+        cowDiv.innerHTML = "I am a " + cow.species + "<br>";
+        dogDiv.innerHTML = "I am a " + dog.species + "<br>";
+        catDiv.innerHTML = "I am a " + cat.species + "<br>";
+        chickenDiv.innerHTML = "I am a " + chicken.species + "<br>";
+        sheepDiv.innerHTML = "I am a " + sheep.species + "<br>";
     }
 
-    
+
     function feedAnimals(): void {
+
+        console.log("button pressed");
 
         console.log("I ate " + animals[0].amount + "kg of " + animals[0].food);
         console.log("I ate " + animals[1].amount + "kg of " + animals[1].food);
@@ -73,6 +87,47 @@ namespace L09_Farm {
         console.log("I ate " + animals[3].amount + "kg of " + animals[3].food);
         console.log("I ate " + animals[4].amount + "kg of " + animals[4].food);
 
+        let cowDiv: HTMLElement = document.getElementById("cow");
+        let dogDiv: HTMLElement = document.getElementById("dog");
+        let catDiv: HTMLElement = document.getElementById("cat");
+        let chickenDiv: HTMLElement = document.getElementById("chicken");
+        let sheepDiv: HTMLElement = document.getElementById("sheep");
+
+        cowDiv.innerHTML += "<br>" + "I ate " + animals[0].amount + "kg of " + animals[0].food + "<br>";
+        dogDiv.innerHTML += "<br>" + "I ate " + animals[1].amount + "kg of " + animals[1].food + "<br>";
+        catDiv.innerHTML += "<br>" + "I ate " + animals[2].amount + "kg of " + animals[2].food + "<br>";
+        chickenDiv.innerHTML += "<br>" + "I ate " + animals[3].amount + "kg of " + animals[3].food + "<br>";
+        sheepDiv.innerHTML += "<br>" + "I ate " + animals[4].amount + "kg of " + animals[4].food + "<br>";
+
+        feedAnimals();
+        displayStorage();
+
+    }
+
+
+
+    function singAnimals(): void {
+
+        console.log("Old Mc Donalds had a farm. " + animals[0].sound + " " + animals[0].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[1].sound + " " + animals[1].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[2].sound + " " + animals[2].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[3].sound + " " + animals[3].sound);
+        console.log("Old Mc Donalds had a farm. " + animals[4].sound + " " + animals[4].sound);
+
+        let cowDiv: HTMLElement = document.getElementById("cow");
+        let dogDiv: HTMLElement = document.getElementById("dog");
+        let catDiv: HTMLElement = document.getElementById("cat");
+        let chickenDiv: HTMLElement = document.getElementById("chicken");
+        let sheepDiv: HTMLElement = document.getElementById("sheep");
+
+        cowDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[0].sound + " " + animals[0].sound;
+        dogDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[1].sound + " " + animals[1].sound;
+        catDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[2].sound + " " + animals[2].sound;
+        chickenDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[3].sound + " " + animals[3].sound;
+        sheepDiv.innerHTML += "<br>" + "Old Mc Donalds had a farm. " + animals[4].sound + " " + animals[4].sound;
+    }
+
+    function displayStorage(): void {
 
         storageLeft.hay -= 50;
         console.log("You have " + storageLeft.hay + " kg hay left.");
@@ -85,19 +140,25 @@ namespace L09_Farm {
         storageLeft.grass -= 10;
         console.log("You have " + storageLeft.grass + " kg grass left.");
 
+        let storageDiv: HTMLElement = document.getElementById("storage");
+        storageDiv.innerHTML = "Old Mc Donalds, here is what you have left:" + "<br>" + "<br>" + storageLeft.hay + " kg of hay " + "<br>" + storageLeft.meat + " kg of meat " + "<br>" + storageLeft.mice + " mice " + "<br>" + storageLeft.corn + " kg of corn " + "<br>" + storageLeft.grass + " kg of grass" + "<br>";
 
+        if (storageLeft.hay  <= 0) {
+            alert("Get more hay!");
+        }
+        if (storageLeft.meat <= 0) {
+            alert("Buy more meat!");
+        }
+        if (storageLeft.mice <= 0) {
+            alert("Catch more mice!");
+        }
+        if (storageLeft.corn <= 0) {
+            alert("Grow more corn!");
+        }
+        if (storageLeft.corn <= 0) {
+            alert("Get more grass!");
+        }
     }
-
-    function singAnimals(): void {
-
-        console.log("Old Mc Donalds had a farm. " + animals[0].sound + " " + animals[0].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[1].sound + " " + animals[1].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[2].sound + " " + animals[2].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[3].sound + " " + animals[3].sound);
-        console.log("Old Mc Donalds had a farm. " + animals[4].sound + " " + animals[4].sound);
-
-
-    }
-
 
 }
+
